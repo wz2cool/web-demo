69e872dab45a5031ff1456ac11c7d50492433e2c
wz2cool <wz2cool@live.cn>
2018-07-31 14:37:06 +0800

    add

--- shared-worker/js/web-worker.js
+++ shared-worker/js/web-worker.js
@@ -1,8 +1,12 @@
 onconnect = function (e) {
     var port = e.ports[0];
-    port.postMessage('client connect: ' + port);
-    port.onmessage = function (e) {
-        port.postMessage('pong'); // not e.ports[0].postMessage!
-        // e.target.postMessage('pong'); would work also
-    }
+    port.addEventListener('message', function (e) {
+        port.postMessage('You said: ' + e.data);
+    }, false);
+    port.start();
+    // port.postMessage('client connect: ' + port);
+    // port.onmessage = function (e) {
+    //     port.postMessage('pong'); // not e.ports[0].postMessage!
+    //     // e.target.postMessage('pong'); would work also
+    // }
 }
