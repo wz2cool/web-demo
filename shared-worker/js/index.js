a378eace2973dbdf235ac42db196d071128a1583
wz2cool <wz2cool@live.cn>
2018-07-31 14:05:17 +0800

    add

---
+++ shared-worker/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var inputMessage = $("#input_message");
+    var inputSend = $("#input_send");
+    inputSend.on('click', sendMessage);
+
+    var path = './js/web-worker.js?v=' + new Date().getTime();
+    var worker = new Worker(path);
+    worker.onmessage = function (event) {
+        console.log('Received message ' + event.data);
+        doSomehing();
+    }
+
+    function sendMessage() {
+        var message = inputMessage.val();
+        worker.postMessage(message);
+    }
+
+    function doSomehing() {
+        console.log('work done!');
+    }
+});
