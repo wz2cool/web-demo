ddf745d1fa851851fb8c4f284d01c75cdc07f36c
wz2cool <wz2cool@live.cn>
2018-08-03 15:17:18 +0800

    add

--- shared-worker-communication/js/index.js
+++ shared-worker-communication/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var channel = new MessageChannel();
     var worker1 = new Worker("js/web-worker.js");
-    var worker2 = new Worker("js/shared-web-worker.js");
+    var worker2 = new SharedWorker("js/shared-web-worker.js");
 
     // Setup the connection: Port 1 is for worker 1
     worker1.postMessage({
@@ -9,7 +9,7 @@ $(document).ready(function () {
     }, [channel.port1]);
 
     // Setup the connection: Port 2 is for worker 2
-    worker2.postMessage({
+    worker2.port.postMessage({
         command: "connect",
     }, [channel.port2]);
 
--- shared-worker-communication/js/shared-web-worker.js
+++ shared-worker-communication/js/shared-web-worker.js
@@ -1,27 +1,31 @@
-var worker1port;
-var onMessageFromWorker1 = function (event) {
-    console.log("Worker 2 received a message from worker 1: " + event.data);
+onconnect = function (e) {
+    var port = e.ports[0];
+    var worker1port;
+    var onMessageFromWorker1 = function (event) {
+        console.log("Worker 2 received a message from worker 1: " + event.data);
 
-    //To send something back to worker 1
-    //worker1port.postMessage("");
-};
+        //To send something back to worker 1
+        //worker1port.postMessage("");
+    };
 
-self.onmessage = function (event) {
-    switch (event.data.command) {
-        // Setup connection to worker 1
-        case "connect":
-            worker1port = event.ports[0];
-            worker1port.onmessage = onMessageFromWorker1;
-            break;
+    port.onmessage = function (event) {
+        switch (event.data.command) {
+            // Setup connection to worker 1
+            case "connect":
+                console.log('hllll');
+                worker1port = event.ports[0];
+                worker1port.onmessage = onMessageFromWorker1;
+                break;
 
-            // Forward messages to worker 1
-        case "forward":
-            // Forward messages to worker 1
-            worker1port.postMessage(event.data.message);
-            break;
+                // Forward messages to worker 1
+            case "forward":
+                // Forward messages to worker 1
+                worker1port.postMessage(event.data.message);
+                break;
 
-            //handle other messages from main
-        default:
-            console.log(event.data);
-    }
-};
+                //handle other messages from main
+            default:
+                console.log(event.data);
+        }
+    };
+}
